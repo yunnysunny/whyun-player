2d189964e7f172c08107307b3660e44d8ebb76ca
yunnysunny <yunnysunny@gmail.com>
2020-06-26 13:29:16 +0800

    完成webcomponent版的hls播放

--- electron/app.js
+++ electron/app.js
@@ -32,7 +32,7 @@ function createWindow() {
     });
 
     // 加载首页
-    mainWindow.loadURL('file://' + __dirname + '/../demo/jquery/demo.html#debug=1');
+    mainWindow.loadURL('file://' + __dirname + '/../demo/webcomponent/demo.html');
 
     // 创建窗体时打开控制台
     //   mainWindow.webContents.openDevTools()
